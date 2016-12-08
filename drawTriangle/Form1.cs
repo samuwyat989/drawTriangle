500c6eedf560b36784a28c5d4ac27430360e2e1f
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-12-08 13:33:59 -0500

    Add project files.

---
+++ drawTriangle/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawTriangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            DrawTriangle();
+        }
+        public void DrawTriangle()
+        {
+            Pen pen = new Pen(Color.Black);
+            int x1, x2, x3, y1, y2, y3;
+            x1 = 100;
+            y1 = 100;
+            x2 = 300;
+            y2 = 100;
+            x3 = 200;
+            y3 = 200;
+        }
+    }
+}
